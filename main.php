d50d82b88df262dd71d8d76043b33fe99d132431
Dominik Klapuch <klapuchdominik@gmail.com>
2017-03-24 15:51:40 +0100

    Debug mode

--- main.php
+++ main.php
@@ -7,8 +7,12 @@ define('CONFIG_FILE', DATA_DIRECTORY . DIRECTORY_SEPARATOR . 'config.json');
 
 $config = json_decode(file_get_contents(CONFIG_FILE), true);
 $parameters = $config['parameters'];
+var_dump($config);
 
 list($url, $username, $password) = [$parameters['url'], $parameters['username'], $parameters['password']];
+var_dump($url);
+var_dump($username);
+var_dump($password);
 
 $options = [
 	'http' => [
